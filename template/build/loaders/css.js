842ed592b3dbc5e864a27b26dfafbdf93815a227
Val.istar.Guo <val.istar.guo@gmail.com>
2018-11-15 23:33:43 +0800

    build(css): enable postcss stage 0

--- template/build/loaders/css.js
+++ template/build/loaders/css.js
@@ -4,7 +4,7 @@ const postcssLoader = {
   loader: 'postcss-loader',
   options: {
     ident: 'postcss',
-    plugins: () => [ postcssPresetEnv() ],
+    plugins: () => [ postcssPresetEnv({ stage: 0 }) ],
   },
 }
 
