bfa8216e529ef1c988a5e5bed773afee538fa32b
Val.istar.Guo <val.istar.guo@gmail.com>
2018-11-22 20:07:57 +0800

    build: differentiate from svg image and font

--- template/build/loaders/font.js
+++ template/build/loaders/font.js
@@ -3,4 +3,8 @@ const urlLoader = {
   options: { limit: 10000 },
 };
 
-export default { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/, use: [urlLoader] }
+export default {
+  test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+  exclude: [/image/, /img/, /images/],
+  use: [urlLoader],
+}
