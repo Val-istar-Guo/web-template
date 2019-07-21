fc3a5b8e2a03a4c600d64c6b44f83190a1c76e83
Val.istar.Guo <val.istar.guo@gmail.com>
2019-07-21 16:47:25 +0800

    fix: lint error when use decorator of typeorm

--- template/client/store/index.js
+++ template/client/store/index.js
@@ -1,13 +1,8 @@
-import env from 'detect-env'
-
+import env from '@framewrok/env'
 import * as MUTATIONS from '../contants/mutations'
-import { FETCH_STATUS } from '../contants/status'
-
-
-const fetch = () => new Promise(resolve => setTimeout(resolve, 4000))
 
 export default {
-  strict: env.is.prod ? false : true,
+  strict: env.is.env,
 
   modules: {
   },
@@ -18,26 +13,9 @@ export default {
   },
 
   actions: {
-    fetchValue: async ({ state, commit }, payload) => {
-      // needless dispatch
-      // if (state.value === payload) return
-
-      console.log('fetching value', payload)
-      commit(MUTATIONS.UPDATE_FETCH_STATE, FETCH_STATUS.FETCHING)
-      await fetch()
-      console.log('fetched value', payload)
-      commit(MUTATIONS.UPDATE_FETCH_STATE, FETCH_STATUS.FETCHED)
-      commit(MUTATIONS.UPDATE_VALUE, payload)
-    }
   },
 
   mutations: {
-    [MUTATIONS.UPDATE_VALUE](state, payload) {
-      state.value = payload
-    },
-    [MUTATIONS.UPDATE_FETCH_STATE](state, payload) {
-      state.isload = payload === FETCH_STATUS.FETCHED
-    },
   },
 
 }
